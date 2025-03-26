0850292da25e59bf147014b23e5a49da591d5c52
eigo-sugiyama <eigo.sugiyama0403@gmail.com>
2025-03-26 13:46:39 +0900

    add .gitattributes warning: in the working copy of '.gitignore', LF will be replaced by CRLF the next time Git touches it 上記のような警告メッセージが発生 これは、現在そのファイルは LF（Line Feed, \n） という改行コードを使っているけど、 Gitの設定により、次にGitがそのファイルに触れたときには CRLF（Carriage Return + Line Feed, \r\n） に変換されるよ、という警告です。 Windowsは通常 CRLF を使いますが、Unix系（LinuxやmacOS）では LF を使うのが一般的です。
    .gitattributesに以下を記述することで、警告を出させないようにする（改行コードを統一)
    *.sh text eol=lf
    *.gitignore text eol=lf
    *.gitattributes text eol=lf

--- rm-cash.sh
+++ rm-cash.sh
@@ -1,3 +1,6 @@
+# 機密情報を含む可能性のあるファイルのキャッシュを削除
+# この操作を行わないと.gitignoreに記載されているファイルでも、以前にコミットしたファイルが残ってしまう
+
 git rm -r --cached ./debugging-with-vscode/obj
 git rm -r --cached ./unit-testing-using-dotnet-test/PrimeService/obj
 git rm -r --cached ./unit-testing-using-dotnet-test/PrimeService.Tests/obj
--- shearch.sh
+++ shearch.sh
@@ -1,2 +1,4 @@
+# 機密情報を含む可能性のあるファイルを検索する
+# 使い方: bash ./shearch.sh
 find . -name "obj"
 find . -name "bin"
